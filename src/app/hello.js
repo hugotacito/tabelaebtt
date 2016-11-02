dc7518bdd40ace8403715687710b38c12c9cb7f1
Hugo Tacito <hugotacito@gmail.com>
2016-11-02 18:56:09 -0300

    corrigindo alguns problemas

--- src/app/hello.js
+++ src/app/hello.js
@@ -6,11 +6,13 @@ export class Hello extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      filterText: '',
-      inStockOnly: false
+      filterText: {}
     };
+    this.handleUserInput = this.handleUserInput.bind(this);
   }
-
+  handleUserInput(formData) {
+    this.setState({formData});
+  }
   render() {
     return (
       <div>
@@ -24,10 +26,10 @@ export class Hello extends React.Component {
         <Grid>
           <Row className="show-grid">
             <Col xs={12} md={6}>
-              <Formulario/>
+              <Formulario formData={this.state.formData} onUserInput={this.handleUserInput}/>
             </Col>
             <Col xs={12} md={6}>
-              <Resultado/>
+              <Resultado formData={this.state.formData} onUserInput={this.handleUserInput}/>
             </Col>
           </Row>
         </Grid>
